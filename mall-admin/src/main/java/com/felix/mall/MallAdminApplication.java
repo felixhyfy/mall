f47e7d74b5ad575e5bb39e7b08901a5349d50fe0
Felix <423126323@qq.com>
2021-01-09 16:40:53 +0800

    bugfix: 去除.iml文件

---
+++ mall-admin/src/main/java/com/felix/mall/MallAdminApplication.java
@@ -0,0 +1,11 @@
+package com.felix.mall;
+
+/**
+ * @author Felix
+ * @packageName com.felix.mall
+ * @className MallAdminApplication
+ * @description
+ * @date 2021/1/9 16:38
+ */
+public class MallAdminApplication {
+}
