0cd424beb47a5ec3c5ba56e507179442fc0d6f78
Felix <423126323@qq.com>
2021-01-09 16:43:05 +0800

    bugfix: 去除所有.iml文件

--- mall-admin/src/main/java/com/felix/mall/MallAdminApplication.java
+++ mall-admin/src/main/java/com/felix/mall/MallAdminApplication.java
@@ -1,11 +1,19 @@
 package com.felix.mall;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author Felix
  * @packageName com.felix.mall
  * @className MallAdminApplication
- * @description
+ * @description 应用启动入口
  * @date 2021/1/9 16:38
  */
+@SpringBootApplication
 public class MallAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallAdminApplication.class, args);
+    }
 }
