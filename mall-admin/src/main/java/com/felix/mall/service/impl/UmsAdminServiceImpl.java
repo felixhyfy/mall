7cd5645a4f43a50221e674fe9d133fb640f5958a
Felix <423126323@qq.com>
2021-01-10 20:31:00 +0800

    init: UmsAdminRoleRelationDao

--- mall-admin/src/main/java/com/felix/mall/service/impl/UmsAdminServiceImpl.java
+++ mall-admin/src/main/java/com/felix/mall/service/impl/UmsAdminServiceImpl.java
@@ -2,12 +2,18 @@ package com.felix.mall.service.impl;
 
 import com.felix.mall.entity.UmsAdmin;
 import com.felix.mall.entity.UmsPermission;
+import com.felix.mall.mapper.UmsAdminMapper;
+import com.felix.mall.security.util.JWTTokenUtil;
 import com.felix.mall.service.UmsAdminService;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.logging.Logger;
+
 
 /**
  * @author Felix
@@ -20,6 +26,33 @@ import java.util.logging.Logger;
 public class UmsAdminServiceImpl implements UmsAdminService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(UmsAdminServiceImpl.class);
+
+    /**
+     * Security的UserDetailsService
+     */
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    /**
+     * 生成和解析Jwt Token的工具类
+     */
+    @Autowired
+    private JWTTokenUtil jwtTokenUtil;
+
+    /**
+     * 密码编码解码类
+     */
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    /**
+     * UmsAdminMapper
+     */
+    @Autowired
+    private UmsAdminMapper adminMapper;
+
+
+
 
     @Override
     public UmsAdmin getAdminByUsername(String username) {
